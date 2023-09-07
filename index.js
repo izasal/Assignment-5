9e06927dc8bfab6635a0d9ed0c521e5aa9e191ee
izasal <142022950+izasal@users.noreply.github.com>
2023-09-07 19:48:17 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,29 @@
+// function sound(){
+//     var snd = new Audio("xmassong.mp3");
+//     snd.play();
+// }
+
+
+let star = "";
+for (let i = 0; i < 10; i++){
+    document.getElementById("new_sample").innerHTML += star + "<br>";
+    star = star + "*";
+}
+
+
+function lights(){
+let red = Math.floor(Math.random() * 256);
+let green = Math.floor(Math.random() * 256);
+let blue = Math.floor(Math.random() * 256);
+let redbluegreen = "rgb(" + red + "," +green+ "," + blue + ")";
+document.body.style.color = redbluegreen;
+}
+
+
+function start(){
+    go = setInterval(lights, 1000);
+}
+
+function stop(){
+    clearInterval(go);
+}
